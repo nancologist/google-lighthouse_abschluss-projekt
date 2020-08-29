83a4144b75b884cc73b472f4ac33ef35c560c3ac
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-08-30 01:05:19 +0200

    # PT2 : successfully run a test with Lighthouse

--- prototype2/index.js
+++ prototype2/index.js
@@ -2,8 +2,26 @@ const { app, BrowserWindow } = require('electron');
 const { ipcMain } = require('electron')
 
 ipcMain.on('xxx', (event, arg) => {
-    console.log(arg) // prints "ping"
-    // event.reply('asynchronous-reply', 'pong')
+    const url = arg;
+    const fs = require('fs');
+    const lighthouse = require('lighthouse');
+    const chromeLauncher = require('chrome-launcher');
+
+    (async () => {
+        const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
+        const options = {logLevel: 'info', output: 'html', onlyCategories: ['performance'], port: chrome.port};
+        const runnerResult = await lighthouse('http://www.nancologist.com', options);
+
+        // `.report` is the HTML report as a string
+        const reportHtml = runnerResult.report;
+        fs.writeFileSync('lhreport.html', reportHtml);
+
+        // `.lhr` is the Lighthouse Result as a JS object
+        console.log('Report is done for', runnerResult.lhr.finalUrl);
+        console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
+
+        await chrome.kill();
+    })();
 })
 
 function createWindow () {
