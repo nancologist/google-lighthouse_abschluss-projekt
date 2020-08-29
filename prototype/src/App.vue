759ec70a8fb669e1943e4d8857aae47b621809e7
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-08-29 17:38:57 +0200

    Apply eslint

--- prototype/src/App.vue
+++ prototype/src/App.vue
@@ -13,7 +13,7 @@ export default {
     components: {
         playground
     },
-    data () {
+    data() {
         return {
             myName: 'David'
         };
--- prototype/src/components/playground/Playground.vue
+++ prototype/src/components/playground/Playground.vue
@@ -1,14 +1,14 @@
 <script>
 export default (context) => {
-    const { props } = context
+    const { props } = context;
     return (
         <div id="playground">
             <h3>Child Component</h3>
             <p>Name in Playground:<strong> { props.name }</strong></p>
             <button vOn:click={props.callFunc}>Reverse Name</button>
         </div>
-    )
-}
+    );
+};
 
 // ****** JSX - even shorter, just like in React ******
 // export default ({ props }) => <h1 class="red">Hallo { props.name }!</h1>
