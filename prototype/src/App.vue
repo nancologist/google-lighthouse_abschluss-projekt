3d271cf4d3af5ed0ba7a1966293c34211ed5b860
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-08-30 01:16:57 +0200

    # PT1 : Pre testing lighthouse

--- prototype/src/App.vue
+++ prototype/src/App.vue
@@ -1,12 +1,32 @@
 <template>
-    <h3>hi...</h3>
+    <div class="main-window">
+        <h1>Test a website</h1>
+        <form class="main-window__form">
+            <label for="url">Select a website: </label>
+            <input id="url" type="text" v-model.lazy="url">
+            <button @click.prevent="testUrl">test</button>
+        </form>
+    </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            url: ''
+        };
+    },
+    methods: {
+        async testUrl() {
+
+        }
+    }
 };
 </script>
 
 <style>
-
+    .main-window {
+        margin: 10px 20px;
+        text-align: center;
+    }
 </style>
