9e6f2c2979626d042a783918707413eeec357895
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-08-31 23:46:16 +0200

    # PT1 : Remove my text arguments against nodeIntegration true

--- prototype/vue.config.js
+++ prototype/vue.config.js
@@ -1,18 +1,7 @@
 module.exports = {
     pluginOptions: {
         electronBuilder: {
-            /*
-            * Mori:
-            * <<< PRELOAD.JS INSTEAD OF NODEINTEGRATION: TRUE >>>
-            * nodeIntegration is set to false since ElectronJS 5+, and it
-            *  prevents that file systems get exposed to the node packages
-            *  and script.
-            *  Specially it could be dangerous if the package connects to
-            *  the internet, so for the security reasons we use preload.js
-            *  and there we expose (import) only what we need through "window"
-            *  object.
-            * */
-            // nodeIntegration: true,
+            nodeIntegration: true,
 
             /*
             * Mori:
