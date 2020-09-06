e389aa28d10f54debda0e998bd4c42d072b38584
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-09-06 20:38:11 +0200

    # PT1 : Edit Stepper

--- prototype/src/vue/components/AuditForm.vue
+++ prototype/src/vue/components/AuditForm.vue
@@ -1,47 +1,63 @@
 <template>
     <div>
-        <v-stepper class="stepper" v-model="currentStep" vertical>
-            <v-stepper-step :complete="currentStep > 1" step="1" editable>
-                Select URL
-                <small>Summarize if needed</small>
-            </v-stepper-step>
+        <v-stepper class="stepper" v-model="currentStep">
+            <v-stepper-header>
+                <v-stepper-step :complete="currentStep > 1" step="1" editable>
+                    Select URL
+                </v-stepper-step>
+                <v-divider/>
+
+                <v-stepper-step :complete="currentStep > 2" step="2" editable>
+                    Setup configuration
+                </v-stepper-step>
+                <v-divider/>
+
+                <v-stepper-step step="3" editable>
+                    Confirm setup
+                </v-stepper-step>
+            </v-stepper-header>
 
             <v-stepper-content step="1">
-                <v-text-field
-                    @focus="initUrlField"
-                    filled
-                    label="Enter URL"
-                    v-model.lazy="auditForm.url"
-                />
-                <v-select
-                    label="Select report format"
-                    v-model="auditForm.reportFormat"
-                    :items="formats"
-                />
-                <v-btn @click="currentStep = 2">Next Step</v-btn>
+                <div class="stepper__content">
+                    <v-text-field
+                        @focus="initUrlField"
+                        filled
+                        label="Enter URL"
+                        v-model.lazy="auditForm.url"
+                    />
+                    <v-select
+                        label="Select report format"
+                        v-model="auditForm.reportFormat"
+                        :items="formats"
+                    />
+                </div>
+                <div class="stepper__btn">
+                    <v-btn class="stepper__content__btn-next" @click="currentStep = 2">Next Step</v-btn>
+                </div>
             </v-stepper-content>
 
-            <v-stepper-step :complete="currentStep > 2" step="2" editable>
-                Setup configuration
-            </v-stepper-step>
-
-            <v-stepper-content step="2">
-                <!-- Todo: Find a way to disable hover effect of switch button (it's very ugly!) -->
-                <v-switch
-                    :label="'Use custom configuration?'"
-                    v-model="auditForm.isCustom"
-                />
-                <v-btn @click="currentStep = 1">Back</v-btn>
-                <v-btn @click="currentStep = 3">Next Step</v-btn>
+            <v-stepper-content class="stepper__content" step="2">
+                <div class="stepper__content">
+                    <!-- Todo: Find a way to disable hover effect of switch button (it's very ugly!) -->
+                    <v-switch
+                        :label="'Use custom configuration?'"
+                        v-model="auditForm.isCustom"
+                    />
+                </div>
+                <div class="stepper__btn">
+                    <v-btn @click="currentStep = 1">Back</v-btn>
+                    <v-btn @click="currentStep = 3">Next Step</v-btn>
+                </div>
             </v-stepper-content>
 
-            <v-stepper-step step="3">
-                Confirm setup
-            </v-stepper-step>
-            <v-stepper-content step="3">
-                <p>Lorem ipsum dolor sit amet.</p>
-                <v-btn @click="currentStep = 2">Back</v-btn>
-                <v-btn @click.prevent="testUrl">Run Test</v-btn>
+            <v-stepper-content class="stepper__content" step="3">
+                <div class="stepper__content">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div class="stepper__btn">
+                    <v-btn @click="currentStep = 2">Back</v-btn>
+                    <v-btn @click.prevent="testUrl">Run Test</v-btn>
+                </div>
             </v-stepper-content>
         </v-stepper>
     </div>
@@ -82,6 +98,11 @@ export default {
     .stepper {
         background-color: var(--third-color) !important;
         box-shadow: none;
+        height: 100vh;
         padding: 10px;
+    }
+
+    .stepper__content {
+        height: 60%;
     }
 </style>
