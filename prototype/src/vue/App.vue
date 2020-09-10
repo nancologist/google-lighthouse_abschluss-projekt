5d7d68b3fb16933e27f2105f2c3e65a5ed80b40d
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-09-10 22:58:38 +0200

    kk

--- prototype/src/vue/App.vue
+++ prototype/src/vue/App.vue
@@ -3,7 +3,7 @@
         <div class="layout"> <!-- Todo: Maybe add a Layout cmp ? (but for now it's very thin and ok here) -->
             <div class="main-window">
                 <template v-if="!appStarted">
-                    <v-btn @click="appStarted = true" >Start App</v-btn>
+                    <v-btn @click="appStarted = true" >Start</v-btn>
                     <div class="powered-by">
                         <span style="display: block; margin-bottom: 10px">_____ Powered By _____</span>
                         <v-icon color="white">mdi-electron-framework</v-icon>
