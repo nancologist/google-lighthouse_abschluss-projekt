8c34e537a54a53db1e02184c384060fd3018102a
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-09-10 18:56:58 +0200

    checkpoint

--- prototype/src/vue/components/AuditForm/Report/Report.vue
+++ prototype/src/vue/components/AuditForm/Report/Report.vue
@@ -1,25 +1,24 @@
 <template>
     <div class="report scrollbar">
-        <div class="report__header">
-            <h2>Test Report</h2>
-            <v-btn
-                class="report__header__close-btn"
-                @click="$emit('close')"
-                icon
-            >
-                <v-icon color="danger">mdi-close-circle</v-icon>
-            </v-btn>
-            <hr>
-        </div>
+<!--        <div class="report__header">-->
+<!--            <v-btn-->
+<!--                class="report__header__close-btn"-->
+<!--                @click="$emit('close')"-->
+<!--                icon-->
+<!--            >-->
+<!--                <v-icon color="danger">mdi-close-circle</v-icon>-->
+<!--            </v-btn>-->
+<!--        </div>-->
         <div class="report__content">
             <!-- If isPowertest = true, "reports" are an array of "audits" for multiple URLs -->
             <template v-if="isPowertest">
                 <v-tabs
                     active-class="active-tab"
+                    show-arrows
+                    slider-color="secondary"
                 >
-                    <v-tabs-slider color="secondary"/>
                     <v-tab v-for="(_, count) in reports" :key="count">
-                        URL {{ count }}
+                        URL {{ count + 1 }}
                     </v-tab>
                     <v-tab-item v-for="audits in reports" :key="audits.url">
                         <h3 style="color: var(--second-color)">{{ audits.url }}</h3>
@@ -29,6 +28,13 @@
                             :audit="audit"
                         />
                     </v-tab-item>
+                    <v-btn
+                        class="report__header__close-btn"
+                        @click="$emit('close')"
+                        icon
+                    >
+                        <v-icon color="danger">mdi-close-circle</v-icon>
+                    </v-btn>
                 </v-tabs>
             </template>
             <!-- If isPowertest = false, actually "reports" are "audits" of a single URL -->
@@ -52,26 +58,26 @@ export default {
 
 <style scoped>
 .report {
-    background-color: #eee;
+    background-color: white;
     border-top-left-radius: 5px;
     border-top-right-radius: 5px;
     color: var(--first-color);
     overflow: scroll;
     text-align: center;
-    padding-top: 10px;
+    /*padding-top: 10px;*/
     padding-bottom: 10px;
 }
 
-.report__header {
-    position: sticky;
+.sticky {
+    position: fixed;
     top: 0;
     width: 100%;
-    background-color: inherit;
+    background-color: white;
 }
 
 .report__header__close-btn {
     position: absolute;
-    top: 0;
+    top: 10%;
     right: 0;
 }
 
