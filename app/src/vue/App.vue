9980fcd3c7094be42777d4d580b27a1807af870c
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-09-21 11:44:18 +0200

    Outsource PoweredBy.

--- app/src/vue/App.vue
+++ app/src/vue/App.vue
@@ -2,17 +2,24 @@
     <v-app>
         <div class="layout"> <!-- Todo: Maybe add a Layout cmp ? (but for now it's very thin and ok here) -->
             <div class="main-window">
+                <v-snackbar
+                    color="danger"
+                    v-model="notConnected"
+                    :timeout="-1"
+                    top
+                    elevation="10"
+                >
+                    <v-icon style="margin-right: 10px">mdi-wifi-strength-off-outline</v-icon>
+                    <span>No internet connection detected.</span>
+                </v-snackbar>
                 <template v-if="!appStarted">
-                    <v-btn @click="appStarted = true" >Start</v-btn>
-                    <div class="powered-by">
-                        <span style="display: block; margin-bottom: 10px">_____ Powered By _____</span>
-                        <v-icon color="white">mdi-electron-framework</v-icon>
-                        <v-icon color="white">mdi-vuejs</v-icon>
-                        <v-icon color="white">mdi-google-chrome</v-icon>
-                        <v-icon color="white">mdi-vuetify</v-icon>
-                        <v-icon color="white">mdi-lighthouse</v-icon>
-                        <v-icon color="white">mdi-nodejs</v-icon>
-                    </div>
+                    <v-btn
+                        @click="appStarted = true"
+                        :disabled="notConnected"
+                    >
+                        Start
+                    </v-btn>
+                    <PoweredBy/>
                 </template>
                 <AuditForm v-else />
             </div>
@@ -22,13 +29,15 @@
 
 <script>
 import AuditForm from './components/AuditForm/AuditForm.vue';
+import PoweredBy from './components/PoweredBy/PoweredBy.vue';
 export default {
-    components: { AuditForm },
+    components: { AuditForm, PoweredBy },
     data() {
         return {
             appStarted: false,
+            notConnected: !navigator.onLine,
         };
-    }
+    },
 };
 </script>
 
@@ -39,14 +48,5 @@ export default {
         margin: auto;
         text-align: center;
         width: 100%;
-    }
-
-    .powered-by {
-        bottom: 2%;
-        color: white;
-        left: 0;
-        margin-bottom: 5px;
-        position: absolute;
-        right: 0;
     }
 </style>
---
+++ app/src/vue/components/PoweredBy/PoweredBy.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="powered-by">
+        <span style="display: block; margin-bottom: 10px">_____ Powered By _____</span>
+        <v-icon color="white">mdi-electron-framework</v-icon>
+        <v-icon color="white">mdi-vuejs</v-icon>
+        <v-icon color="white">mdi-google-chrome</v-icon>
+        <v-icon color="white">mdi-vuetify</v-icon>
+        <v-icon color="white">mdi-lighthouse</v-icon>
+        <v-icon color="white">mdi-nodejs</v-icon>
+    </div>
+</template>
+
+<style scoped>
+    .powered-by {
+        bottom: 2%;
+        color: white;
+        left: 0;
+        margin-bottom: 5px;
+        position: absolute;
+        right: 0;
+    }
+</style>
